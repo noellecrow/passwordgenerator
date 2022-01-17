5511e9623b6e6df445c3c008966f1e65bc522427
noellecrow <crownoelle@gmail.com>
2022-01-16 20:45:07 -0800

    add variables

--- Develop/script.js
+++ Develop/script.js
@@ -5,6 +5,16 @@
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
+
+// Define variables
+var charSelection = "";
+var passwordLength = 7;
+var a = 0;
+var lowercaseAlpha = "abcdefghijklmnopqrstuvwxyz";
+var uppercaseAlpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var numeric = "0123456789";
+var special = "!@#$%^&*()_-=+|';:/?.>,<`~";
+var passwordVal = ""
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
